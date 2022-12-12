ed958f0d4e94a6bec05f057c203ac20e977f98ca
Sharmin Moon <sharminmoon0308@gmail.com>
2022-12-12 18:33:26 +0600

    rent house and rent flat are implemented

--- pages/rent/flat/index copy.js
+++ pages/rent/flat/index copy.js
@@ -10,51 +10,28 @@ const RentFlat = () => {
     {
       id: "1",
       title: "Single Family flat",
-      size: `2 bed, 2 bath 1,555sqft`,
+      size: `3 bed, 2 bath 1,555sqft`,
       description: `Brand new construction single family home!!! Located in a huge
       corner lot, this could be your dream home. 3 bedrooms and 2 full
       bathrooms. Move in ready with brand new appliances included. 2 car
       garage, garage door opener with remote. Ring doorbell.`,
 
       price: "$2,100/month",
-      location: "Dhaka,Mirpur",
+      location: "767 Staffora St, Haines City, FL 33844",
       image: "/images/flat-1.jpeg",
     },
     {
       id: "2",
       title: "Flat For Single Family",
-      size: "2 bed ,2 bath, 1,281sqft",
+      size: "3 bed ,3 bath, 1,381sqft",
       description: ` Come look at this beautiful two-story home nestled in a cul- de-sac,
             conveniently located between highway 90 & 410, only minutes away
             from Lackland AFB! This home features an open floor plan with all
-            bedrooms & laundry room upstairs!`,
-      price: "$1,450/month",
-      location: "Dhaka, Dhanmondi",
+            bedrooms & laundry room upstairs! Available for immediate move in,
+            contact us today to schedule your showing!`,
+      price: "$1,750/month",
+      location: "7516 Bowdre, San Antonio, TX 78252",
       image: "/images/flat-2.jpeg",
-    },
-    {
-      id: "3",
-      title: "Single Family Luxary Flat",
-      size: "3 bed ,3 bath, 2,000sqft",
-      description: ` Come look at this beautiful two-story home nestled in a cul- de-sac,
-            conveniently located between highway 90 & 410, only minutes away
-            from Lackland AFB! This home features an open floor plan with all
-            bedrooms & laundry room upstairs!`,
-      price: "$2050/month",
-      location: "Chittagong",
-      image: "/images/flat-3.jpeg",
-    },
-    {
-      id: "4",
-      title: "Flat For Single Family",
-      size: "2 bed ,2 bath, 1,500sqft",
-      description: ` Come look at this beautiful two-story home nestled in a cul- de-sac,
-            conveniently located between highway 90 & 410, only minutes away
-            from Lackland AFB! This home features an open floor plan with all
-            bedrooms & laundry room upstairs!`,
-      price: "$1,650/month",
-      location: "Khulna",
-      image: "/images/flat-4.png",
     },
   ];
 
@@ -71,7 +48,7 @@ const RentFlat = () => {
             <p className="mb-5 text-2xl font-bold">
               Discover your perfect flat, Search nearby flat for rent
             </p>
-            <button className="btn btn-warning">Explore</button>
+            <button className="btn btn-success">Get Started</button>
           </div>
         </div>
       </div>
