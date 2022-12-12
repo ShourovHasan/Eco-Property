39c6912c5948fe22638a7f41ebb88ff2f9d82c31
ShourovHasan <shourovhasan@gmail.com>
2022-12-12 17:38:30 +0600

    Home page and contact updated

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Footer from '../components/Footer/Footer'
+import Advertise from '../components/Home/Advertise/Advertise'
+import Banner from '../components/Home/Banner/Banner'
 import Navbar from '../components/Navbar/Navbar'
 import styles from '../styles/Home.module.css'
-
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Navbar></Navbar>
-
+      <Advertise></Advertise>
+      <Banner></Banner>
       <Footer></Footer>
     </div>
   )
